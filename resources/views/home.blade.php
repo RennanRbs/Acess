3d52eb4f13d6cf9f743ceaf70b6915e64b94b4b5
Rennan Rbs <rennanrbs@gmail.com>
2017-02-09 13:32:18 -0300

    1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                     Você Logou no Lit
+                      <div><a href="{{ url('/usuario') }}">Entrar na Página do Usuário</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/usuario.blade.php
@@ -0,0 +1,25 @@
+<html lang="pt-br">
+<head><link rel="stylesheet" type "text/css" href="_css/estilo.css"/></head>
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                     bem vindo a Rede Lit
+                      <div id="imagem">
+                          <img src="{{ URL::to('/') }}/imagens/getway.png"/>
+                      </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+</html>
